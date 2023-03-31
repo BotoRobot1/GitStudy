12913fd4ec0c6b23e4569afe62086f99f0844e4a
Денис <zkansler@bk.ru>
2023-03-31 11:30:20 +0700

    Выполнил первое ДЗ

---
+++ src/main/java/org/example/Lesson1/HomeWork_1.java
@@ -0,0 +1,12 @@
+package org.example.Lesson1;
+
+public class HomeWork_1 {
+    public static void main(String[] args) {
+        task1();
+
+    }
+
+    private static void task1(){
+        System.out.println("Решение первой задачи");
+    }
+}
